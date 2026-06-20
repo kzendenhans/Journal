ab2d5684359a1b97e696f0fa8b0f5604b6527202
Hans <Hans@MacBook-Pro.local>
2026-06-20 15:06:16 +0200

    clear all caches, disable SW caching

--- sw.js
+++ sw.js
@@ -1,19 +1,6 @@
-const CACHE = 'dagboek-v3';
-const ASSETS = ['/Journal/', '/Journal/index.html', '/Journal/style.css', '/Journal/app.js', '/Journal/manifest.json'];
-
-self.addEventListener('install', e => e.waitUntil(
-  caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-));
-
+// Wis alle oude caches en doe verder niets
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(
-    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-  )).then(() => self.clients.claim())
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
 ));
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
